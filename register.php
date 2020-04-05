c89569b5bc16f1404253644d71e69a9dd5c45e73
FarisBektas <faris.bektas@stu.ibu.edu.ba>
2020-04-05 17:00:00 +0200

    initial comment

---
+++ register.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet" type="text/css" href="scss/style.css" />
+<script type="text/javascript" language="javascript" src="javascripts/jquery.js"></script>
+<script type="text/javascript" language="javascript" src="javascripts/script.js"></script>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+<title>Konfirmacija registracije</title>
+</head>
+
+<body>
+
+<div class="logo"></div>
+
+<div class="workarea">
+
+<h1>Uspjesno ste se registrovali</h1>
+
+</div>
+
+</body>
+</html>
